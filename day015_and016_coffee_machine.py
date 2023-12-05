70da1284841b1594240d8bee7f71ca941e3f10a5
MalgSemik <malg.sem@gmail.com>
2023-12-05 12:24:07 +0100

    Apply code review suggestions

--- day015_and016_coffee_machine.py
+++ day015_and016_coffee_machine.py
@@ -7,11 +7,18 @@ MENU = {
     },
 }
 
+COINS = {
+    "quarter": 0.25,
+    "dime": 0.1,
+    "nickle": 0.05,
+    "penny": 0.01
+}
+
 resources = {"water": 300, "milk": 200, "coffee": 100}
 
 
 class CoffeeMachine:
-    def __init__(self):
+    def __init__(self, resources):
         self.resources = resources
         self.profit = 0
 
@@ -29,33 +36,26 @@ class CoffeeMachine:
         print("The coffee machine will now turn off.")
 
     def _check_resources(self, order):
-        if (
-            self.resources["water"] >= MENU[order]["ingredients"]["water"]
-            and self.resources["coffee"] >= MENU[order]["ingredients"]["coffee"]
-        ):
-            if "milk" in MENU[order]["ingredients"].keys():
-                if self.resources["milk"] >= MENU[order]["ingredients"]["milk"]:
-                    return True
-            else:
-                return True
-        return False
+        for ingredient in MENU[order]["ingredients"].keys():
+            if self.resources[ingredient] < MENU[order]["ingredients"][ingredient]:
+                return False
+        return True
 
-    def _accept_coins(self):
-        num_quarters = int(input("How many quarters? "))
-        num_dimes = int(input("How many dimes? "))
-        num_nickles = int(input("How many nickles? "))
-        num_pennies = int(input("How many pennies? "))
-        return [num_quarters, num_dimes, num_nickles, num_pennies]
+    def _make_payment(self):
+        inserted_amount = 0
+        for coin, value in COINS.items():
+            inserted_amount += int(input(f"How many {coin}s? ")) * value # TODO: it should print pennies not pennys but I don't know how to do it
+        print(inserted_amount)
+        return inserted_amount
 
-    def _calculate_value(self):
-        coins = self._accept_coins()
-        value = coins[0] * 0.25 + coins[1] * 0.1 + coins[2] * 0.05 + coins[3] * 0.01
-        return value
-
-    def _check_transaction_successful(self, order):
-        if self._calculate_value() >= MENU[order]["cost"]:
+    def _make_transaction(self, order):
+        value = self._make_payment()
+        if value >= MENU[order]["cost"]:
             self.profit += MENU[order]["cost"]
             return True
+        else:
+            print("Not enough money.")
+            return False
 
     def _new_order(self):
         new_order = input("Would you like to make another order? Type 'y' or 'n': ")
@@ -66,27 +66,23 @@ class CoffeeMachine:
 
     def _make_coffee(self, order):
         print(f"Making {order} in progress...")
-        self.resources["water"] -= MENU[order]["ingredients"]["water"]
-        self.resources["coffee"] -= MENU[order]["ingredients"]["coffee"]
-        if "milk" in MENU[order]["ingredients"].keys():
-            self.resources["milk"] -= MENU[order]["ingredients"]["milk"]
-
+        for ingredient in MENU[order]["ingredients"].keys():
+            self.resources[ingredient] -= MENU[order]["ingredients"][ingredient]
         print("Your coffee is done. Please collect your order.")
         self._new_order()
 
     def get_order(self):
-        order = input("What would you like? (espresso/latte/cappuccino): ")
+        order = input(f"What would you like? {tuple(MENU.keys())}: ")
         if order == "report":
             self._print_report()
             self._new_order()
         elif order == "off":
             self._turn_off()
-        elif order in ["espresso", "latte", "cappuccino"]:
+        elif order in MENU:
             if self._check_resources(order):
-                if self._check_transaction_successful(order):
+                if self._make_transaction(order):
                     self._make_coffee(order)
                 else:
-                    print("Not enough money.")
                     self._new_order()
             else:
                 print("Not enough resources.")
@@ -96,5 +92,5 @@ class CoffeeMachine:
             self.get_order()
 
 
-cm = CoffeeMachine()
+cm = CoffeeMachine(resources)
 cm.get_order()
