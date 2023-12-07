9a763c218f8e10cd38eae74dd0e568f17051d4f0
MalgSemik <malg.sem@gmail.com>
2023-12-07 16:44:34 +0100

    Add initial files

---
+++ day023_turtle_crossing/player.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.left(90)
+        self.shape("turtle")
+        self.color("green")
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def down(self):
+        self.backward(MOVE_DISTANCE)
+
+    def go_left(self):
+        self.goto(self.xcor()-10, self.ycor())
+
+    def go_right(self):
+        self.goto(self.xcor()+10, self.ycor())
+
