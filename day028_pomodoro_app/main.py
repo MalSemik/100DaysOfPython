0a2eb43d99028e39c7ff90237b214821f2739377
MalgSemik <malg.sem@gmail.com>
2023-12-14 15:16:44 +0100

    Add functionalities

--- day028_pomodoro_app/main.py
+++ day028_pomodoro_app/main.py
@@ -1,3 +1,4 @@
+import winsound
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,21 +10,59 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✓"
+reps = 0
+timer = None
 
-
-def start():
-    print("Start")
-
-
-def reset():
-    print("Reset")
 
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(countdown_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    progress_label.config(text="")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps, CHECKMARK
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(countdown_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        winsound.Beep(440, 1000)
+        marks = ""
+        work_sessions = reps // 2
+        for i in range(work_sessions):
+            marks += CHECKMARK
+        progress_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # create window
@@ -35,15 +74,15 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+countdown_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 # create labels
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-progress_label = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
+progress_label = Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
 
 # create buttons
-start_button = Button(text="Start", command=start)
-reset_button = Button(text="Reset", command=reset)
+start_button = Button(text="Start", command=start_timer)
+reset_button = Button(text="Reset", command=reset_timer)
 
 # create layout
 timer_label.grid(column=1, row=0)
